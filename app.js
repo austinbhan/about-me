ae51a68fb91dd0f4ca43076f720843e3cc694d3f
Austin Han <austinbhan@outlook.com>
2022-04-20 15:13:19 -0700

    Finished Wireframe in .html

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
-// import functions
-
 // grab DOM elements
+const button = document.getElementById('button');
+const nameEl = document.getElementById('name');
+const sectionEl = document.getElementById('my-info');
+const animalDiv = document.getElementById('animal-div');
+console.log('animalDiv', animalDiv);
 
+const animalButton = document.getElementById('animal-button');
 // set event listeners 
+button.addEventListener('click', () => {
+    if (nameEl.textContent === '____') {
+        nameEl.textContent = 'Austin';
+    } else {
+        nameEl.textContent = '_____';
+    }
+    nameEl.textContent = 'Austin';
+    sectionEl.classList.toggle('my-style');
+});
+
+animalButton.addEventListener('click', () => {
+    animalDiv.classList.remove('hidden');
+});
     // get user input
     // use user input to update state 
     // update DOM to reflect the new state
+

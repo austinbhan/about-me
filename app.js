248e1655a58681e71c40e939999b87e6896a0d83
Austin Han <austinbhan@outlook.com>
2022-04-20 15:32:16 -0700

    Added const value to DOM elements

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // grab DOM elements
-document.getElementById('whoAmIButton');
-document.getElementById('whereImFromButton');
-document.getElementById('whatILikeButton');
+const whoAmIButton = document.getElementById('whoAmIButton');
+const whereImFromButton = document.getElementById('whereImFromButton');
+const whatILikeButton = document.getElementById('whatILikeButton');
 // set event listeners 
     // get user input
     // use user input to update state 

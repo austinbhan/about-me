261fd603ba9cc300bbf2b6213e698edd487182a9
Austin Han <austinbhan@outlook.com>
2022-04-20 16:26:03 -0700

    Added functional buttons

--- app.js
+++ app.js
@@ -2,11 +2,24 @@
 const whoAmIButton = document.getElementById('whoAmIButton');
 const whereImFromButton = document.getElementById('whereImFromButton');
 const whatILikeButton = document.getElementById('whatILikeButton');
+
+const whoAmIDiv = document.getElementById('whoAmIDiv');
+const whereImFromDiv = document.getElementById('whereImFromDiv');
+const whatILikeDiv = document.getElementById('whatILikeDiv');
 // set event listeners 
 
+whoAmIButton.addEventListener('click', () => {
+    whoAmIDiv.classList.remove('hidden');
+});
 
+whereImFromButton.addEventListener('click', () => {
+    whereImFromDiv.classList.remove('hidden');
+});
+
+whatILikeButton.addEventListener('click', () => {
+    whatILikeDiv.classList.remove('hidden');
+});
 
     // get user input
     // use user input to update state 
     // update DOM to reflect the new state
-

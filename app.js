7c1a1c4f331d3bf4134fb92762d7c3edac631add
Austin Han <austinbhan@outlook.com>
2022-04-20 15:39:06 -0700

    Added .hidden class to <p> attributes

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const whoAmIButton = document.getElementById('whoAmIButton');
 const whereImFromButton = document.getElementById('whereImFromButton');
 const whatILikeButton = document.getElementById('whatILikeButton');
 // set event listeners 
+
+
+
     // get user input
     // use user input to update state 
     // update DOM to reflect the new state

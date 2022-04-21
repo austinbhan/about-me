1a744a71aeb3baff07fd47158593c5442926936e
Austin Han <austinbhan@outlook.com>
2022-04-20 17:20:36 -0700

    Buttons now working

--- app.js
+++ app.js
@@ -1,21 +1,15 @@
 // grab DOM elements
 const whoAmIButton = document.getElementById('whoAmIButton');
-const whereImFromButton = document.getElementById('whereImFromButton');
 const whatILikeButton = document.getElementById('whatILikeButton');
 const animalButton = document.getElementById('animalButton');
 
 const whoAmIDiv = document.getElementById('whoAmIDiv');
-const whereImFromDiv = document.getElementById('whereImFromDiv');
 const whatILikeDiv = document.getElementById('whatILikeDiv');
-const gooseDiv = document.getElementById.apply('untitledGoose');
+const gooseDiv = document.getElementById('untitledGoose');
 // set event listeners 
 
 whoAmIButton.addEventListener('click', () => {
     whoAmIDiv.classList.remove('hidden');
-});
-
-whereImFromButton.addEventListener('click', () => {
-    whereImFromDiv.classList.remove('hidden');
 });
 
 whatILikeButton.addEventListener('click', () => {
